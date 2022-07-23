ca896458e50609091cedc3948c1c5c400dddd15b
vemerion <vemerion@gmail.com>
2022-07-23 21:58:35 +0200

    Add language provider

---
+++ src/main/java/mod/vemerion/livingitems/datagen/ModLanguageProvider.java
@@ -0,0 +1,18 @@
+package mod.vemerion.livingitems.datagen;
+
+import mod.vemerion.livingitems.Main;
+import mod.vemerion.livingitems.init.ModEntities;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.LanguageProvider;
+
+public class ModLanguageProvider extends LanguageProvider {
+
+	public ModLanguageProvider(DataGenerator gen) {
+		super(gen, Main.MODID, "en_us");
+	}
+
+	@Override
+	protected void addTranslations() {
+		add(ModEntities.LIVING_ITEM.get(), "Living Item");
+	}
+}
